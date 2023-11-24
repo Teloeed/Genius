15e19807795c17276d07def686b114f6f205a87d
Teloeed <komandor415565@gmail.com>
2023-11-24 14:54:31 +0200

    HomeWork 4

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
-// let message = "Mishanya kon'";
-// alert(message);
-// const isConfirm = confirm('Ti pro?');
-// let name = prompt('Enter your name');
-// console.log(3 % 2);
-let a = 10;
-switch (a) {
-  case 3:
-    console.log(3);
-    break;
-  case 5:
-    console.log(5);
-    break;
-  case 7:
-    console.log(7);
-    break;
-  case 9:
-    console.log(9);
-    break;
-  case 10:
-    console.log(10);
-    break;
-  default:
-    console.log('default');
-}
-console.log(a ** a);
+//код для чтение числа из окна и выведения в консоль
+
+/* console.log(document);
+
+const inputRef = document.querySelector('input[name="number"]');
+const buttonRef = document.querySelector('button');
+buttonRef.addEventListener('click', () => {
+  console.log(inputRef.value);
+});
+console.log(inputRef.value); */
+
+console.log(document);
+
+const addNumber = number => {
+  let summ = Number(number) + 10;
+  return console.log(summ);
+};
+
+const inputRef = document.querySelector('input[name="number"]');
+const buttonRef = document.querySelector('button');
+buttonRef.addEventListener('click', () => {
+  addNumber(inputRef.value);
+});
